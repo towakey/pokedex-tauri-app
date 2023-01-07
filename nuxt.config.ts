c4733e9d867a7d52b0920863f3c3316ed60f6ead
towakey <kyosgame@gmail.com>
2023-01-07 19:05:16 +0900

    設定変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from "nuxt/config";
+import { Static } from "nuxt/dist/app/compat/capi";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
   css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
   build: {
     transpile: ["vuetify"],
