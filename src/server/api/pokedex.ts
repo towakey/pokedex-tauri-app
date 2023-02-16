dcb8afc33f237472b2809701fe5a4e5368b9bbea
towakey <kyosgame@gmail.com>
2023-02-16 23:42:50 +0900

    コメントの削除

--- src/server/api/pokedex.ts
+++ src/server/api/pokedex.ts
@@ -100,37 +100,12 @@ export default defineEventHandler(async (event) => {
         var globalNo
         var localNo
 
-        // globalNo = areaPokedex["pokedex"][area][id-1]["globalNo"]
-        // pokedex["no"] = areaPokedex["pokedex"][area][id-1]["no"]
-        // pokedex["globalNo"] = areaPokedex["pokedex"][area][id-1]["globalNo"]
-        // pokedex["name"] = global["pokedex"][Number(globalNo)-1]["name"]["jpn"]
-        // pokedex["height"] = global["pokedex"][Number(globalNo)-1]["height"]
-        // pokedex["weight"] = global["pokedex"][Number(globalNo)-1]["weight"]
-        // pokedex["classification"] = global["pokedex"][Number(globalNo)-1]["classification"]
-        // pokedex["type1"] = areaPokedex["pokedex"][area][id-1]["status"][0]["type1"]
-        // pokedex["type2"] = areaPokedex["pokedex"][area][id-1]["status"][0]["type2"]
-        // pokedex["ability1"] = areaPokedex["pokedex"][area][id-1]["status"][0]["ability1"]
-        // pokedex["ability2"] = areaPokedex["pokedex"][area][id-1]["status"][0]["ability2"]
-        // pokedex["dream_ability"] = areaPokedex["pokedex"][area][id-1]["status"][0]["dream_ability"]
-        // pokedex["hp"] = areaPokedex["pokedex"][area][id-1]["status"][0]["hp"]
-        // pokedex["attack"] = areaPokedex["pokedex"][area][id-1]["status"][0]["attack"]
-        // pokedex["defense"] = areaPokedex["pokedex"][area][id-1]["status"][0]["defense"]
-        // pokedex["special_attack"] = areaPokedex["pokedex"][area][id-1]["status"][0]["special_attack"]
-        // pokedex["special_defense"] = areaPokedex["pokedex"][area][id-1]["status"][0]["special_defense"]
-        // pokedex["speed"] = areaPokedex["pokedex"][area][id-1]["status"][0]["speed"]
-        // result = pokedex
-
-        // console.log("ID:"+id)
-
         pokedex = {}
         localNo = Number(id - 2)
-        // console.log("0:"+localNo)
         if(localNo == -1){
           result[0]=""
-          // console.log("0:"+globalNo)
         }else{
           globalNo = Number(areaPokedex["pokedex"][area][id - 2]["globalNo"] - 1)
-          // console.log("0:"+globalNo)
           pokedex["no"] = areaPokedex["pokedex"][area][localNo]["no"]
           pokedex["globalNo"] = areaPokedex["pokedex"][area][localNo]["globalNo"]
           pokedex["name"] = global["pokedex"][globalNo]["name"]["jpn"]
@@ -154,8 +129,6 @@ export default defineEventHandler(async (event) => {
         pokedex = {}
         localNo = Number(id - 1)
         globalNo = Number(areaPokedex["pokedex"][area][id - 1]["globalNo"] - 1)
-        // console.log("1:"+localNo)
-        // console.log("1:"+globalNo)
 
         pokedex["no"] = areaPokedex["pokedex"][area][localNo]["no"]
         pokedex["globalNo"] = areaPokedex["pokedex"][area][localNo]["globalNo"]
@@ -178,13 +151,10 @@ export default defineEventHandler(async (event) => {
 
         pokedex = {}
         localNo = Number(id)
-        // console.log("2:"+localNo)
         if(localNo >= indexMax){
           result[2]=""
-          // console.log("2(true):"+globalNo)
         }else{
           globalNo = Number(areaPokedex["pokedex"][area][id]["globalNo"] - 1)
-          // console.log("2(false):"+globalNo)
           pokedex["no"] = areaPokedex["pokedex"][area][localNo]["no"]
           pokedex["globalNo"] = areaPokedex["pokedex"][area][localNo]["globalNo"]
           pokedex["name"] = global["pokedex"][globalNo]["name"]["jpn"]
