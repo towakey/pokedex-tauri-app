99c14d504579319b37b04deba8b0e4929fb3fd39
towakey <kyosgame@gmail.com>
2023-03-05 00:53:59 +0900

    detailviewにjson呼び出しを移動

--- src/components/DetailsView.vue
+++ src/components/DetailsView.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
-  defineProps(["pokedexArea", "pokedexName", "pokedate", "siteTitle"])
+  const props = defineProps(["pokedexArea", "pokedexName", "id"])
+
+  const loadPokedex = await useFetch('/api/pokedex?id='+props.id+'&area='+props.pokedexArea+'&type=details', { refresh: true })
+  const pokedate = loadPokedex.data.value.pokedex
+
+  const metaTitle = ref(pokedate[1].name+" - "+props.pokedexName)
+
+useHead({
+  title: metaTitle,
+  meta: [
+  {
+      hid: 'og:title',
+      name: 'og:title',
+      content: metaTitle
+    },
+    {
+      hid: 'twitter:card',
+      name: 'twitter:card',
+      content: 'summary'
+    },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: metaTitle
+    }
+  ]
+})
+
 </script>
 <template>
   <LinkView :pokedexArea="pokedexArea" :pokedexName="pokedexName" :pokedate="pokedate" />
@@ -7,5 +34,5 @@
   <TypeView :type1="pokedate[1].type1" :type2="pokedate[1].type2" />
   <StatusChart :statusData="pokedate[1]" />
   <AbilityView :ability1="pokedate[1].ability1" :ability2="pokedate[1].ability2" :dream_ability="pokedate[1].dream_ability" />
-  <SnsView :siteTitle="siteTitle" />
+  <SnsView :siteTitle="metaTitle" />
 </template>
--- src/pages/pokedex/unova_b2w2/[id].vue
+++ src/pages/pokedex/unova_b2w2/[id].vue
@@ -7,34 +7,10 @@ const pokedexArea = "unova_b2w2"
 const pokedexName = "イッシュ図鑑"
 
 const { id } = route.params;
-const loadPokedex = await useFetch('/api/pokedex?id='+id+'&area='+pokedexArea+'&type=details', { refresh: true })
-const pokedate = loadPokedex.data.value.pokedex
 
-const metaTitle = ref(pokedate[1].name+" - "+pokedexName)
-
-useHead({
-  title: metaTitle,
-  meta: [
-  {
-      hid: 'og:title',
-      name: 'og:title',
-      content: metaTitle
-    },
-    {
-      hid: 'twitter:card',
-      name: 'twitter:card',
-      content: 'summary'
-    },
-    {
-      hid: 'twitter:title',
-      name: 'twitter:title',
-      content: metaTitle
-    }
-  ]
-})
 </script>
 <template>
-  <DetailsView :pokedexArea="pokedexArea" :pokedexName="pokedexName" :pokedate="pokedate" :siteTitle="metaTitle" />
+  <DetailsView :pokedexArea="pokedexArea" :pokedexName="pokedexName" :id="id" />
 </template>
 <style>
 </style>
