cdbbbec027d1beaab92f8bff937d47e1b111e486
towakey <kyosgame@gmail.com>
2023-03-21 23:04:14 +0900

    faviconを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,16 @@ export default defineNuxtConfig({
   target: 'static',
   ssr: true,
   app: {
-    // head: {
-    //   title: "Pokedex Online",
-    // },
+    head: {
+      title: "Pokedex Online",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+      ]
+    },
     // baseURL: '/public'
   },
   css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
