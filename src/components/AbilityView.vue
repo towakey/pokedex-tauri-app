04798ca40ea28f8e68216e0023cfbcba656ac260
towakey <kyosgame@gmail.com>
2023-06-11 23:12:15 +0900

    不要行の削除

--- src/components/AbilityView.vue
+++ src/components/AbilityView.vue
@@ -1,24 +1,6 @@
 <script setup lang="ts">
-  const props = defineProps(["ability1", "ability2", "dream_ability", "pokedexArea", "pokedate"])
-
-  const gameList: { [key: string]: string } = {
-    "kanto": "Red_Green_Blue_Yellow",
-    "johto": "Gold_Silver_Crystal",
-    "hoenn": "Ruby_Sapphire_Emerald",
-    "sinnoh": "Diamond_Pearl_Platinum",
-    "unova_bw": "Black_White",
-    "unova_b2w2": "Black2_White2",
-    "central_kalos": "X_Y",
-    "coast_kalos": "X_Y",
-    "mountain_kalos": "X_Y",
-    "alola_sm": "Sun_Moon",
-    "alola_usum": "UltraSun_UltraMoon",
-    "galar": "Sword_Shield",
-    "isle_of_armor": "Sword_Shield",
-    "crown_tundra": "Sword_Shield",
-    "hisui": "LegendsArceus",
-    "paldea": "Scarlet_Violet",
-  }
+const appConfig = useAppConfig()
+const props = defineProps(["ability1", "ability2", "dream_ability", "pokedexArea", "pokedate"])
 
 </script>
 <template>
