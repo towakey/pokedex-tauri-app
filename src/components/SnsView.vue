8099eb1973588683ae314b4db3dc302f1c06e8d8
towakey <kyosgame@gmail.com>
2023-07-17 14:40:09 +0900

    マストドンシェアボタンを修正

--- src/components/SnsView.vue
+++ src/components/SnsView.vue
@@ -53,8 +53,8 @@
   <v-card
   elevation="0"
   >
-    <v-card-title>
-      <div class="sns_line">
+    <v-card-title overflow-wrap="break-word">
+      <!-- <div class="sns_line"> -->
         <a href="tweetLink"
           target="_blank"
           rel="noopener noreferrer"
@@ -67,12 +67,12 @@
           </svg>
           <span class="twitter-share-button-label">Tweet</span>
         </a>
-      </div>
+      <!-- </div> -->
       <!-- <a :href="getMastodonShareURL" target="_blank" rel="noopener noreferrer">
         共有する
         <font-awesome-icon :icon="['fab', 'mastodon']" />
       </a> -->
-      <div class="sns_line">
+      <!-- <div class="sns_line"> -->
         <div class="mastodon-share-button">
         <div v-if="!isShared">
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M433 179.11c0-97.2-63.71-125.7-63.71-125.7-62.52-28.7-228.56-28.4-290.48 0 0 0-63.72 28.5-63.72 125.7 0 115.7-6.6 259.4 105.63 289.1 40.51 10.7 75.32 13 103.33 11.4 50.81-2.8 79.32-18.1 79.32-18.1l-1.7-36.9s-36.31 11.4-77.12 10.1c-40.41-1.4-83-4.4-89.63-54a102.54 102.54 0 0 1-.9-13.9c85.63 20.9 158.65 9.1 178.75 6.7 56.12-6.7 105-41.3 111.23-72.9 9.8-49.8 9-121.5 9-121.5zm-75.12 125.2h-46.63v-114.2c0-49.7-64-51.6-64 6.9v62.5h-46.33V197c0-58.5-64-56.6-64-6.9v114.2H90.19c0-122.1-5.2-147.9 18.41-175 25.9-28.9 79.82-30.8 103.83 6.1l11.6 19.5 11.6-19.5c24.11-37.1 78.12-34.8 103.83-6.1 23.71 27.3 18.4 53 18.4 175z"/></svg>
@@ -91,14 +91,15 @@
           <button class="mastodon-share-reset-button" @click="resetInstanceURL">リセット</button>
         </div>
         </div>
-      </div>
+      <!-- </div> -->
     </v-card-title>
   </v-card>
 </template>
 <style scoped>
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
 .twitter-share-button {
-  display: inline-flex;
+  display: inline-blockk;
+  width: fit-content;
   align-items: center;
   font-size: 14px;
   font-weight: 500;
@@ -129,12 +130,15 @@
 .mastodon-share-button {
   text-decoration: none;
   border: 1px solid #3c3c3c;
+  background-color: #3c3c3c;
   border-radius: 100px;
-  padding: 8px 16px;
-
+  padding: 4px 16px;
+  display: inline-blockk;
+  width: fit-content;
 }
 .mastodon-share-save-text-field {
   margin-right: 10px;
+  color: white;
 }
 .mastodon-share-save-button {
   background-color: #3c3c3c;
@@ -149,8 +153,8 @@
   color: aliceblue;
   border: 1px solid #3c3c3c;
   margin-left: 10px;
-  padding-left: 10px;
-  padding-right: 10px;
+  padding-left: 1px;
+  padding-right: 1px;
   text-decoration: none;
 }
 .mastodon-share-reset-button {
@@ -162,7 +166,8 @@
   padding-right: 10px;
 }
 .sns_line {
-  display: inline;
+  display: inline-flex;
   margin: 5px;
+  overflow: hidden;
 }
 </style>
